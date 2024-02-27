0f407b3d7dc718d3b0e4919068715842e381cb70
bilalSerwan <ensw02121@student.su.edu.krd>
2024-02-27 20:57:14 +0300

    v3.1

--- controller/course.controller.js
+++ controller/course.controller.js
@@ -16,7 +16,7 @@ class CourseController {
             const isvalid = await CourseValidation.validateCourse(body);
             if (isvalid.status) {
                 try {
-                    
+
                     res.send(await CourseModule.addCourse(body));
                 } catch (e) {
                     console.log(e);
@@ -31,13 +31,12 @@ class CourseController {
         return async (req, res) => {
             const searched = req.params.search;
             const regulerEx = new RegExp('.*' + searched + '.*', 'i');
-            try{
-            res.send(await CourseModule.searchForCourse(regulerEx));
-            }catch(ex){
+            try {
+                res.send(await CourseModule.searchForCourse(regulerEx));
+            } catch (ex) {
                 console.log(ex);
-            } 
+            }
         }
     }
-
 }
 module.exports = new CourseController();
